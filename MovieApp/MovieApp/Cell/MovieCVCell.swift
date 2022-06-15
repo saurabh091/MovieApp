9c886d67e386d7d4ccf1801740b2c99ff16edc62
Saurabh Rana <saurabhrana091@gmail.com>
2022-06-15 17:30:19 +0530

    * Added Collection View Cell * Configure api with collection View

--- MovieApp/MovieApp/Cell/MovieCVCell.swift
+++ MovieApp/MovieApp/Cell/MovieCVCell.swift
@@ -6,8 +6,30 @@
 //
 
 import UIKit
+import SDWebImage
 
 class MovieCVCell: UICollectionViewCell {
+    static let identifier = String(describing: MovieCVCell.self)
     
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet private weak var titleLabel: UILabel!
     
+    // To set name title.
+    var movieName: String? {
+        didSet {
+            titleLabel.isHidden = movieName == nil ? true : false
+            titleLabel.text = titleLabel.isHidden ? "" : movieName
+        }
+    }
+    
+    /// To set amount
+    var moviePoster: String? {
+        didSet {
+            imageView.isHidden = moviePoster == nil ? true : false
+            if let imageURl = moviePoster {
+                let url = URL(string: imageURl)
+                imageView.sd_setImage(with: url, placeholderImage: UIImage.init(named: "placeholder"), options: SDWebImageOptions(rawValue: 0), completed: nil)
+            }
+        }
+    }
 }
--- MovieApp/MovieApp/Controllers/ViewController.swift
+++ MovieApp/MovieApp/Controllers/ViewController.swift
@@ -6,12 +6,13 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
     
     
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var movieListCollectionView: UICollectionView!
     private var viewModel: ViewModel?
     private var movieArray: [Movie] = []
     
@@ -19,7 +20,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         viewModel = ViewModel()
-        setupView()
         getMovieList()
     }
     
@@ -30,28 +30,40 @@ class ViewController: UIViewController {
                 return
             }
             self.movieArray = list.search.count > 0 ? list.search : []
+            DispatchQueue.main.async {
+                self.movieListCollectionView.reloadData()
+                self.movieListCollectionView.layoutIfNeeded()
+            }
             debugPrint("Movie Lsit: \(self.movieArray)")
         })
-    }
-    
-    func setupView() {
-        collectionView.register(UICollectionViewCell.self,
-                                forCellWithReuseIdentifier: "MovieCVCell")
-        collectionView.delegate = self
-        collectionView.dataSource = self
     }
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return movieArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCVCell",
-//                                                      for: indexPath) as! MovieCVCell
-//        return cell
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MovieCVCell.identifier,
+                                                      for: indexPath) as! MovieCVCell
+        cell.movieName = movieArray[indexPath.row].title
+        cell.moviePoster = movieArray[indexPath.row].poster
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let height = view.frame.size.height
+        let width = view.frame.size.width
+        // in case you you want the cell to be 40% of your controllers view
+        return CGSize(width: width * 0.46, height: height * 0.4)
     }
 }
 
